98d1748ff3ba8d4a9cc4b6c499711e222d46fd91
Maximilian Arnold <arnoldma80620@th-nuernberg.de>
2026-02-26 21:51:05 +0100

    dyn node now dependes on async service

--- shared/dyn_node/examples/processor.rs
+++ shared/dyn_node/examples/processor.rs
@@ -1,42 +1,76 @@
-use bevy::prelude::*;
-use dyn_node::prelude::*;
-use on_asset_loaded::prelude::*;
+use std::result::Result;
+
+use async_service::*;
+use bevy::{ecs::system::RunSystemOnce, prelude::*};
+use dyn_node::{QueryError, prelude::*};
 use serde::Deserialize;
 
-#[derive(Deserialize, Asset, TypePath, Component)]
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(AsyncServicePlugin)
+        .add_plugins(DynNodePlugin)
+        .init_asset::<Enemy>()
+        .init_resource::<Enemies>()
+        .add_systems(Update, print_enemies)
+        .run();
+}
+
+#[derive(Deserialize, Asset, TypePath)]
 struct Enemy {
     health: u32,
     damage: u32,
     speed: u32,
 }
 
-fn main() {
-    App::new()
-        .add_plugins(DefaultPlugins)
-        .add_plugins(AssetObserverPlugin)
-        .add_plugins(DynNodePlugin)
-        .init_dyn_asset::<Enemy>()
-        .add_systems(Startup, setup)
-        .run();
+#[derive(Resource, Deref)]
+struct Enemies(Vec<Handle<Enemy>>);
+
+impl FromWorld for Enemies {
+    fn from_world(world: &mut World) -> Self {
+        let enemies = world.run_system_once(get_enemies).unwrap();
+        Enemies(enemies)
+    }
 }
 
-fn setup(
+fn get_enemies(
     asset_server: Res<AssetServer>,
-    mut dyn_resolver: DynNodeResolver,
-    mut commands: Commands,
-) {
-    let h_dyn = asset_server.load("config.yml");
-
+    async_service: Res<AsyncService>,
+) -> Vec<Handle<Enemy>> {
+    let handle = asset_server.load::<DynNode>("config.yml");
     ["/enemies/aligator", "/enemies/ork", "/enemies/goblin"]
         .iter()
-        .for_each(|path| {
-            let h_enemy = dyn_resolver.resolve(h_dyn.clone(), path.to_string());
-
-            commands.on_loaded(h_enemy, |input: OnLoaded<Enemy>| {
-                println!(
-                    "Enemy loaded: health={}, damage={}, speed={}",
-                    input.asset.health, input.asset.damage, input.asset.speed
-                );
-            });
+        .map(|path| {
+            asset_server.add_async({
+                let async_service = async_service.clone();
+                let asset_server = asset_server.clone();
+                let h_dyn = handle.clone();
+                async move {
+                    asset_server.wait_for_asset(&h_dyn).await.unwrap();
+                    async_service
+                        .exec_sync(query_node_sync, (h_dyn, path.to_string()))
+                        .await
+                }
+            })
         })
+        .collect()
+}
+
+fn query_node_sync(
+    In((h, path)): In<(Handle<DynNode>, String)>,
+    nodes: Res<Assets<DynNode>>,
+) -> Result<Enemy, QueryError> {
+    nodes.get(&h).unwrap().query::<Enemy>(&path)
+}
+
+fn print_enemies(enemies: Res<Enemies>, assets: Res<Assets<Enemy>>) {
+    enemies
+        .iter()
+        .flat_map(|h| assets.get(h))
+        .for_each(|enemy| {
+            println!(
+                "Enemy: health={}, damage={}, speed={}",
+                enemy.health, enemy.damage, enemy.speed
+            );
+        });
 }
--- shared/dyn_node/src/lib.rs
+++ shared/dyn_node/src/lib.rs
@@ -1,26 +1,19 @@
-use std::{any::TypeId, borrow::Borrow};
-
-use bevy::{
-    asset::AssetHandleProvider, ecs::system::SystemParam, platform::collections::HashMap,
-    prelude::*, utils::TypeIdMap,
-};
+use bevy::prelude::*;
 use derive_more::From;
-use on_asset_loaded::prelude::*;
 use serde::{Deserialize, de::DeserializeOwned};
 use serde_json::Value;
+use thiserror::Error;
+
+pub mod prelude {
+    pub use super::{DynNode, DynNodePlugin};
+}
 
 /// make sure to register after any other asset loaders that are more sepcific than this before this (e.g. ".enemy.yml")
 pub struct DynNodePlugin;
 
-pub mod prelude {
-    pub use super::{AppExt as _, DynNode, DynNodePlugin, DynNodeResolver};
-}
-
 impl Plugin for DynNodePlugin {
     fn build(&self, app: &mut App) {
-        app.init_resource::<HandleProviders>()
-            .init_resource::<HandleRegistry>()
-            .init_dyn_asset::<DynNode>();
+        app.init_asset::<DynNode>();
 
         #[cfg(feature = "cbor")]
         {
@@ -50,22 +43,12 @@ impl Plugin for DynNodePlugin {
     }
 }
 
-pub trait AppExt {
-    /// initialize an asset so it can be deserialized from a [`DynNode`] via a [`DynNodeResolver`].
-    fn init_dyn_asset<A: Asset + DeserializeOwned>(&mut self) -> &mut Self;
-}
-
-impl AppExt for App {
-    fn init_dyn_asset<A: Asset + DeserializeOwned>(&mut self) -> &mut Self {
-        self.init_asset::<A>();
-
-        let hp = self.world().resource::<Assets<A>>().get_handle_provider();
-        self.world_mut()
-            .resource_mut::<HandleProviders>()
-            .insert(TypeId::of::<A>(), hp);
-
-        self
-    }
+#[derive(Debug, Error)]
+pub enum QueryError {
+    #[error("Path not found: {0}")]
+    NotFound(String),
+    #[error("Failed to deserialize value at path: {0} into type: {1}")]
+    DeserializeError(String, String),
 }
 
 /// Generic Config Node format (can be deserialized from json, yaml, ...)
@@ -74,91 +57,16 @@ pub struct DynNode(Value);
 
 impl DynNode {
     /// query a config file at a given path. using json pointer format (starts with /)
-    pub fn query_raw(&self, path: &str) -> Option<&Value> {
-        self.0.pointer(path)
+    pub fn query_raw(&self, path: &str) -> Result<&Value, QueryError> {
+        self.0
+            .pointer(path)
+            .ok_or_else(|| QueryError::NotFound(path.to_string()))
     }
 
     /// query a config file at a given path and deserialize it. using json pointer format (starts with /)
-    pub fn query<T: DeserializeOwned>(&self, path: &str) -> Option<T> {
-        self.query_raw(path)
-            .and_then(|value| serde_json::from_value(value.clone()).ok())
+    pub fn query<T: DeserializeOwned>(&self, path: &str) -> Result<T, QueryError> {
+        let raw = self.query_raw(path)?;
+        serde_json::from_value(raw.clone())
+            .map_err(|e| QueryError::DeserializeError(path.to_string(), e.to_string()))
     }
-}
-
-#[derive(SystemParam)]
-pub struct DynNodeResolver<'w, 's> {
-    registry: ResMut<'w, HandleRegistry>,
-    commands: Commands<'w, 's>,
-    handle_providers: Res<'w, HandleProviders>,
-}
-
-impl DynNodeResolver<'_, '_> {
-    pub fn resolve<A: Asset + DeserializeOwned>(
-        &mut self,
-        h_config: impl Borrow<Handle<DynNode>>,
-        query: String,
-    ) -> Handle<A> {
-        let asset_id = h_config.borrow().id();
-        let type_id = TypeId::of::<A>();
-
-        if let Some(handle) = self.registry.0.get(&(asset_id, query.clone(), type_id)) {
-            return handle.clone().typed();
-        }
-
-        let handle = self
-            .handle_providers
-            .get(&type_id)
-            .expect("ExtractFromConfigPlugin not added for this asset type")
-            .reserve_handle()
-            .typed();
-
-        self.commands.on_loaded_with(
-            h_config.borrow(),
-            (handle.clone(), query),
-            on_loaded_query_extract::<A>,
-        );
-
-        handle
-    }
-}
-
-#[derive(Resource, Default, Deref, DerefMut)]
-struct HandleProviders(TypeIdMap<AssetHandleProvider>);
-
-#[derive(Resource, Default, Deref, DerefMut)]
-struct HandleRegistry(HashMap<(AssetId<DynNode>, String, TypeId), UntypedHandle>);
-
-/// On Asset Loaded Processor that queries a config node for a path and deserializes the result as an asset
-/// Example:
-///
-/// ```
-/// let h_config: Handle<ConfigNode> = asset_server.load("config.yml");
-/// let h_enemy: Handle<Enemy> = enemies.reserve_handle();
-/// commands.on_loaded_with(
-///     &h_config,
-///     (h_enemy.clone(), "/enemies/aligator".to_string()),
-///     query_config_processor::<Enemy>,
-/// );
-/// ```
-///
-/// given config.yml:
-/// ```yaml
-/// enemies:
-///   aligator:
-///     health: 100
-///     damage: 10
-/// ```
-///
-fn on_loaded_query_extract<A: DeserializeOwned + Asset>(
-    input: OnLoaded<DynNode, (Handle<A>, String)>,
-    mut assets: ResMut<Assets<A>>,
-) {
-    let config = input.asset;
-    let out = input.params.0;
-    let path = input.params.1;
-
-    let Some(a) = config.query(&path) else {
-        panic!("Failed to query config for path: {path}");
-    };
-    assets.insert(out.id(), a).expect("Failed to insert asset");
 }
